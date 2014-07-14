caece02413a974dde315cb7e82659866c0a16c23
Doug Wiegley <doug@parksidesoftware.com>
2014-07-13 18:28:39 -0600

    pep8

--- tests/unit/test_virtual_port.py
+++ tests/unit/test_virtual_port.py
@@ -24,21 +24,22 @@ class TestVirtualPort(unittest.TestCase):
     def test_virtual_port_delete(self):
         with mocks.VirtualPortDelete().client() as c:
             c.slb.virtual_server.vport.delete('vip1', 'vip1_VPORT',
-                                      c.slb.virtual_server.vport.HTTP,
-                                      '80')
+                                              c.slb.virtual_server.vport.HTTP,
+                                              '80')
 
     def test_virtual_port_delete_not_found(self):
         with mocks.VirtualPortDeleteNotFound().client() as c:
             c.slb.virtual_server.vport.delete('vip1', 'vip1_VPORT',
-                                      c.slb.virtual_server.vport.HTTP,
-                                      '80')
+                                              c.slb.virtual_server.vport.HTTP,
+                                              '80')
 
     def test_virtual_port_create(self):
         with mocks.VirtualPortCreate().client() as c:
-            c.slb.virtual_server.vport.create('vip1', 'vip1_VPORT',
-                                      protocol=c.slb.virtual_server.vport.HTTP,
-                                      port='80',
-                                      service_group_name='pool1')
+            c.slb.virtual_server.vport.create(
+                'vip1', 'vip1_VPORT',
+                protocol=c.slb.virtual_server.vport.HTTP,
+                port='80',
+                service_group_name='pool1')
 
     def test_virtual_port_create_exists(self):
         with mocks.VirtualPortCreateExists().client() as c:
