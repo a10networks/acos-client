81d9820924284508e319984873ae6ac3fbd37927
Doug Wiegley <dougw@a10networks.com>
2014-07-15 14:50:29 -0600

    nuke pbr, overkill

--- setup.py
+++ setup.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 
-import setuptools
+from setuptools import setup, find_packages
+setup(
+    name = "acos-client",
+    version = "0.3",
+    packages = find_packages(),
 
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+    author = "A10 Networks",
+    author_email = "dougw@a10networks.com",
+    description = "A10 Networks ACOS API Client",
+    license = "Apache",
+    keywords = "a10 axapi acos adc slb load balancer",
+    url = "https://github.com/a10networks/acos-client",
+
+    long_description = open('README.md').read(),
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+
+    install_requires = ['hash_ring>=1.3.1']
+)
