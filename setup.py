cba3bffa6dd83b8bb77121b6f1d7944626e75442
Doug Wiegley <dougw@a10networks.com>
2014-08-15 18:34:18 -0600

    ignore weird close error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "acos-client",
-    version = "0.6.7",
+    version = "0.6.8",
     packages = find_packages(),
 
     author = "A10 Networks",
