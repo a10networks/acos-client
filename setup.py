45c7279b347f4fefd72bbe3bc89518fa0937bfeb
Doug Wiegley <dougw@a10networks.com>
2014-08-20 15:45:21 -0600

    missing client bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "acos-client",
-    version = "1.0.4",
+    version = "1.0.5",
     packages = find_packages(),
 
     author = "A10 Networks",
