0e8175133dc4f693ff7cc96115e0edd1f7ea3366
Shane Johnson <sandplunge@gmail.com>
2014-09-06 14:28:14 -0600

    update unittest2 imports

--- tests/unit/test_system.py
+++ tests/unit/test_system.py
@@ -12,8 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import unittest
-
+import unittest2 as unittest
 import v21_mocks as mocks
 
 
