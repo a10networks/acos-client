9bc1927e54308e43274a1d74fd42aa008e97e4ef
Alon Blayer-Gat <Alon.BlayerGat@gmail.com>
2014-12-09 11:02:27 +0200

    bump minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "acos-client",
-    version = "1.1.0",
+    version = "1.1.1",
     packages = find_packages(),
 
     author = "A10 Networks",
