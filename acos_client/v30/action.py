7e04a56e9c302510db16109c8edeaa206a3e4447
Michael Durrant <mdurrant@a10networks.com>
2016-03-16 18:00:41 +0000

    Modified activate_and_write signature to accept single partition name

--- acos_client/v30/action.py
+++ acos_client/v30/action.py
@@ -25,5 +25,5 @@ class Action(base.BaseV30):
         }
         self._post("/write/memory/", payload, **kwargs)
 
-    def write_active(self, partitions=[], **kwargs):
+    def activate_and_write(self, partition, **kwargs):
         self.write_memory()
