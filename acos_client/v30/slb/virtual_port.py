cb76015bc5ecd6fe6748c108e310addefd7666f6
Michael Durrant <mdurrant@a10networks.com>
2016-03-18 18:27:59 +0000

    Updated per review suggestion

--- acos_client/v30/slb/virtual_port.py
+++ acos_client/v30/slb/virtual_port.py
@@ -81,8 +81,9 @@ class VirtualPort(base.BaseV30):
             }, exclude=exclude_minimize)
         }
 
-        sampling_enable = kwargs.get('sampling_enable', [])
-        self._set_sampling_enable(sampling_enable, params)
+        sampling_enable = kwargs.get('sampling_enable')
+        if sampling_enable is not None:
+            self._set_sampling_enable(sampling_enable, params)
 
         url = self.url_server_tmpl.format(name=virtual_server_name)
         if update:
