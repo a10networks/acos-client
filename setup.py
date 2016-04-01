0052390f63b7386abec00a5c95c3d306ce516972
Doug Wiegley <dougw@a10networks.com>
2016-03-31 21:00:29 -0600

    bump to 1.3.3alpha

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "acos-client",
-    version = "1.3.2",
+    version = "1.3.3alpha",
     packages = find_packages(),
 
     author = "A10 Networks",
