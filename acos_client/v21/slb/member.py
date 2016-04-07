037b234b29fc07dd712501178eeaf6a3e58b224f
Michael Durrant <mdurrant@a10networks.com>
2016-04-07 17:19:45 +0000

    Correct oper implementation v21 members

--- acos_client/v21/slb/member.py
+++ acos_client/v21/slb/member.py
@@ -44,6 +44,7 @@ class Member(base.BaseV21):
                     server_name, int(server_port), **kwargs)
 
     def get_oper(self, service_group_name, server_name, server_port, **kwargs):
-        return self._post("slb.service_group.member.oper", service_group_name,
-                          server_name, int(server_port), **kwargs)
-
+        sg_stats = self._post("slb.service_group.fetchStatistics",
+                              {"name": service_group_name}, **kwargs)
+        members_stats = sg_stats["service_group_stat"]["member_stat_list"]
+        member_stats = filter(lambda x: x["name"] == server_name)
