5b383930a807e36f37229213ca7b5988c1dbdbff
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-05-10 10:28:16 +0900

    Added the implementation to operate port on a real-server in SLB using the aXAPI3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "acos-client",
-    version = "1.4.0",
+    version = "1.4.1",
     packages = find_packages(),
 
     author = "A10 Networks",
