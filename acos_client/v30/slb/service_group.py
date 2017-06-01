c2580dfa261a767ee131f49df2ade7722187332f
Michael Durrant <mdurrant@a10networks.com>
2017-06-01 06:17:07 +0000

    Remove protocol before put

--- acos_client/v30/slb/service_group.py
+++ acos_client/v30/slb/service_group.py
@@ -94,6 +94,8 @@ class ServiceGroup(base.BaseV30):
             name = ''
             self._post(self.url_prefix + name, params, **kwargs)
         else:
+            if 'protocol' in params['service-group']:
+                del params['service-group']['protocol']
             self._put(self.url_prefix + name, params, **kwargs)
 
     def create(self, name, protocol=TCP, lb_method=ROUND_ROBIN, **kwargs):
