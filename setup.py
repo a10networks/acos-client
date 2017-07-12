aa50babf6b67be35bcd4ae118ba1da87ac2cf8ef
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-07-12 11:02:45 +0900

    Reverted increment of version-number for the internal version control

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "acos-client",
-    version = "1.4.1",
+    version = "1.4.0",
     packages = find_packages(),
 
     author = "A10 Networks",
