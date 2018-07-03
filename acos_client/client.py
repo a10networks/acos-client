a7911d628f8b8c1f37dd28b6cceb9aaa76a30931
Hunter Thompson <thompson.grey.hunter@gmail.com>
2018-07-03 21:41:18 +0000

    Removed unavailable code

--- acos_client/client.py
+++ acos_client/client.py
@@ -32,7 +32,6 @@ from acos_client.v21.session import Session as v21_Session
 from acos_client.v21.sflow import SFlow as v21_SFlow
 from acos_client.v21.slb import SLB as v21_SLB
 from acos_client.v21.system import System as v21_System
-from acos_client.v21.vrrpa import VrrpA as v21_VRRPA
 
 from acos_client.v30 import axapi_http as v30_http
 from acos_client.v30.dns import DNS as v30_DNS
@@ -62,7 +61,6 @@ VERSION_IMPORTS = {
         'SFlow': v21_SFlow,
         'SLB': v21_SLB,
         'System': v21_System,
-        'VRRPA': v21_VRRPA
     },
     '30': {
         'DNS': v30_DNS,
