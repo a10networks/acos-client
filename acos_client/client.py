62c156d24f42ecc288df6bead64e05d79baf4a65
Michael Durrant <mdurrant@a10networks.com>
2018-07-13 22:09:44 -0600

    RIB CRUD - no BFD support yet

--- acos_client/client.py
+++ acos_client/client.py
@@ -42,6 +42,7 @@ from acos_client.v30.license_manager import LicenseManager as v30_LicenseManager
 from acos_client.v30.nat import Nat as v30_Nat
 from acos_client.v30.network import Network as v30_Network
 from acos_client.v30.overlay import Overlay as v30_Overlay
+from acos_client.v30.route import RIB as v30_RIB
 from acos_client.v30.session import Session as v30_Session
 from acos_client.v30.sflow import SFlow as v30_SFlow
 from acos_client.v30.slb import SLB as v30_SLB
@@ -72,6 +73,7 @@ VERSION_IMPORTS = {
         'Nat': v30_Nat,
         'Network': v30_Network,
         'Overlay': v30_Overlay,
+        'RIB': v30_RIB,
         'Session': v30_Session,
         'SFlow': v30_SFlow,
         'SLB': v30_SLB,
@@ -145,6 +147,10 @@ class Client(object):
     @property
     def overlay(self):
         return VERSION_IMPORTS[self._version]["Overlay"](self)
+
+    @property
+    def route(self):
+        return VERSION_IMPORTS[self._version]["RIB"](self)
 
     def wait_for_connect(self, max_timeout=60):
         for i in six.moves.range(0, max_timeout):
