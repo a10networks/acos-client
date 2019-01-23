7d79b5b7a25fd9808817073b59cab1246faaa1aa
omkartelee01 <omkareshwar.telee@a10networks.com>
2019-01-23 18:53:44 +0530

    code cleanup

--- acos_client/v30/slb/service_group.py
+++ acos_client/v30/slb/service_group.py
@@ -103,7 +103,6 @@ class ServiceGroup(base.BaseV30):
 
         if not update:
             name = ''
-            #self._post(self.url_prefix + name, params, **kwargs)
         else:
             if 'protocol' in params['service-group']:
                 del params['service-group']['protocol']
