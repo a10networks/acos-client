67d128481f07eea70e8e4598b1a01ff274ec941b
omkartelee01 <omkareshwar.telee@a10networks.com>
2019-01-24 17:53:54 +0530

    corrected page ownership

--- acos_client/tests/unit/v30/test_slb_aflex.py
+++ acos_client/tests/unit/v30/test_slb_aflex.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2016, A10 Networks.
+# Copyright 2014-2016, Omkar Telee, A10 Networks.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
