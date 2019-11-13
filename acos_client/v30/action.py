17477bc9af4c4afe6712c32d0a16b5d2974e3ada
omkartelee01 <omkareshwar.telee@a10networks.com>
2019-11-13 11:09:17 +0530

    added HA code support

--- acos_client/v30/action.py
+++ acos_client/v30/action.py
@@ -96,3 +96,11 @@ class Action(base.BaseV30):
     def vcs_reload(self):
         url = "/vcs/reload"
         self._post(url)
+
+    def check_vrrp_status(self):
+        url = "/vrrp-a"
+        data = self._get(url)
+        if "common" in data["vrrp-a"].keys() and "action" in data["vrrp-a"]["common"].keys() and data["vrrp-a"]["common"]["action"] == "enable":
+            return True
+        else:
+            return False
