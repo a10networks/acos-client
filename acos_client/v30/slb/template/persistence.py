72bd811c18176e3c9330030813bd706beaab3952
omkartelee01 <omkareshwar.telee@a10networks.com>
2020-01-21 12:43:26 +0530

    support for cookie name attribute in app cookie session persistence

--- acos_client/v30/slb/template/persistence.py
+++ acos_client/v30/slb/template/persistence.py
@@ -37,7 +37,8 @@ class BasePersistence(base.BaseV30):
     def create(self, name, **kwargs):
         if self.exists(name):
             raise acos_errors.Exists
-        self._post(self.prefix, self.get_params(name), **kwargs)
+        cookie_name = kwargs.get("cookie_name", None)
+        self._post(self.prefix, self.get_params(name, cookie_name), **kwargs)
 
     def delete(self, name, **kwargs):
         self._delete(self.prefix + name, **kwargs)
@@ -49,10 +50,11 @@ class CookiePersistence(BasePersistence):
         self.pers_type = 'cookie'
         super(CookiePersistence, self).__init__(client)
 
-    def get_params(self, name):
+    def get_params(self, name, cookie_name=None):
         return {
             "cookie": {
-                "name": name
+                "name": name,
+                "cookie-name" : cookie_name
             }
         }
 
@@ -63,7 +65,7 @@ class SourceIpPersistence(BasePersistence):
         self.pers_type = 'source-ip'
         super(SourceIpPersistence, self).__init__(client)
 
-    def get_params(self, name):
+    def get_params(self, name, cookie_name=None):
         return {
             "source-ip": {
                 "name": name
