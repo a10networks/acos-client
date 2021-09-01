e5a2897f678d3aa986466c52076f0ef0840b9294
Rahul katre <rahul.katre@a10networks.com>
2021-09-01 20:28:07 +0530

    Added glm proxy server configuration class to client

--- acos_client/client.py
+++ acos_client/client.py
@@ -40,6 +40,7 @@ from acos_client.v30.device_context import DeviceContext as v30_DeviceContext
 from acos_client.v30.dns import DNS as v30_DNS
 from acos_client.v30.file import File as v30_File
 from acos_client.v30.glm.flexpool import Flexpool as Flexpool
+from acos_client.v30.glm.proxy import ProxyServer
 from acos_client.v30.ha import HA as v30_HA
 from acos_client.v30.interface import Interface as v30_Interface
 from acos_client.v30.license_manager import LicenseManager as v30_LicenseManager
@@ -90,6 +91,7 @@ VERSION_IMPORTS = {
         'DeviceContext': v30_DeviceContext,
         'Flexpool': Flexpool,
         'Delete': Delete,
+        'ProxyServer': ProxyServer,
     },
 }
 
@@ -173,6 +175,13 @@ class Client(object):
             LOG.error("AXAPIv21 is not supported for the glm attribute")
             return
         return VERSION_IMPORTS['30']["Flexpool"](self)
+
+    @property
+    def glm_proxy_server(self):
+        if self._version != '30':
+            LOG.error("AXAPIv21 is not supported for the glm attribute")
+            return
+        return VERSION_IMPORTS['30']["ProxyServer"](self)
 
     @property
     def overlay(self):
