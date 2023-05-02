7a60c02d893f3edfa030d9994fa6e4ba734a5071
ytsai-a10 <ytsai@a10networks.com>
2023-05-02 17:20:15 +0000

    [STACK-3637][STACK-3639] vthunder IPv6 interface and address issues

--- acos_client/v30/action.py
+++ acos_client/v30/action.py
@@ -68,9 +68,13 @@ class Action(base.BaseV30):
         if address_list and ip_version == 4:
             data = {'ethernet': {'ifnum': str(interface), "name": "DataPort", "action": "enable",
                     'ip': {'address-list': address_list}}}
-        if address_list and ip_version == 6:
-            data = {'ethernet': {'ifnum': str(interface), "name": "DataPort",
-                    'ipv6': {'address-list': address_list, 'ipv6-enable': 1}}}
+
+        if ip_version == 6:
+            if address_list:
+                data = {'ethernet': {'ifnum': str(interface), "name": "DataPort", "action": "enable",
+                        'ipv6': {'address-list': address_list, 'ipv6-enable': 1}}}
+            else:
+                data = {'ethernet': {'ifnum': str(interface), "name": "DataPort"}}
 
         url = "/interface/ethernet/" + str(interface)
         self._put(url, data)
